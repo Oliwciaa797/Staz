d25eb6601716ddc43b89b6f0d471b2c2ac3fb4fb
zliemni0r <matyldazasadzka@gmail.com>
2026-07-10 10:32:52 +0200

    haselka

--- rejestracja/register.js
+++ rejestracja/register.js
@@ -1,3 +1,13 @@
 function back() {
     window.location.href = "../strona startowa/start.html";
 }
+function sprawdzHasla() {
+    const haslo1 = document.getElementById("password").value;
+    const haslo2 = document.getElementById("password2").value;
+
+    if (haslo1 === haslo2) {
+        alert("Hasła są takie same.");
+    } else {
+        alert("Hasła nie są takie same!");
+    }
+}
