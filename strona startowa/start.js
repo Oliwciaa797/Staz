2c2192f0c2076ea671480ffd10622d0ae995dc3c
Diana <vattrysh@gmail.com>
2026-07-10 12:35:50 +0200

    filtry i przesuwanie search baru

--- strona startowa/start.js
+++ strona startowa/start.js
@@ -1,16 +1,26 @@
+const input = document.getElementById("searchInput");
+
+input.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        search();
+    }
+});
 function search() {
     const value = document.getElementById("searchInput").value;
 
     if (value.trim() === "") {
         alert("Wpisz coś do wyszukania.");
         return;
+    } else {
+        document.getElementById("mainContent").classList.add("search-mode");
     }
 
-    alert("Szukasz: " + value);
+    console.log("Szukasz: " + value);
 }
 
-function categorySearch() {
-    window.location.href = "../filtrowanie/filt.html";
+function categoryDropdown() {
+    document.getElementById("filtersDropdown").classList.toggle("show");
 }
 
 function goToLogin() {
@@ -28,3 +38,21 @@ document.addEventListener("DOMContentLoaded", () => {
 function toggleMenu() {
     document.getElementById("sidebar").classList.toggle("active");
 }
+
+const slider = document.getElementById("slider");
+const value = document.getElementById("durationValue");
+
+noUiSlider.create(slider, {
+    start: [10, 60],      // od 10 do 60 minut
+    connect: true,
+    range: {
+        min: 0,
+        max: 300
+    },
+    step: 1
+});
+
+slider.noUiSlider.on("update", function(values) {
+    value.textContent =
+        `${Math.round(values[0])} min - ${Math.round(values[1])} min`;
+});
