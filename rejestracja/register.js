2a202dccc6cf9762a04f70ea909e4ef6e38da6e4
zliemni0r <matyldazasadzka@gmail.com>
2026-07-10 10:34:36 +0200

    znowu hasla

--- rejestracja/register.js
+++ rejestracja/register.js
@@ -1,11 +1,11 @@
 function back() {
     window.location.href = "../strona startowa/start.html";
 }
-function sprawdzHasla() {
-    const haslo1 = document.getElementById("password").value;
-    const haslo2 = document.getElementById("password2").value;
+function checkPasswords() {
+    const password1 = document.getElementById("password").value;
+    const password2 = document.getElementById("password2").value;
 
-    if (haslo1 === haslo2) {
+    if (password1 === password2) {
         alert("Hasła są takie same.");
     } else {
         alert("Hasła nie są takie same!");
