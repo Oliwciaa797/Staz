49f29af897457b6a43d7075aa058ea765dbd288b
zliemni0r <matyldazasadzka@gmail.com>
2026-07-10 11:20:02 +0200

    hasla

--- rejestracja/register.js
+++ rejestracja/register.js
@@ -4,11 +4,17 @@ function back() {
 function checkPasswords() {
     const password1 = document.getElementById("password").value;
     const password2 = document.getElementById("password2").value;
+    const error = document.getElementById("passwordError");
 
-    if (password1 === password2) {
-        
-    } else {
-        alert("Hasła nie są takie same!");
+    error.textContent = "";
+
+     if (password1.length < 8) {
+        error.textContent = "Hasło musi mieć co najmniej 8 znaków.";
+        return;
+    }
+    if (password1 !== password2) {
+        error.textContent = "Hasła muszą być takie same.";
+        return;
     }
 }
 function sprawdzCheckbox() {
