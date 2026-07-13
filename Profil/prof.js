0a5fa3015056bb4dbe6a12eab916538514b44173
zliemni0r <matyldazasadzka@gmail.com>
2026-07-13 10:17:33 +0200

    small pointer changes

--- Profil/prof.js
+++ Profil/prof.js
@@ -1,3 +1,7 @@
+function back() {
+    window.location.href = "../strona startowa/start.html";
+}
+
 const SUPABASE_URL = 'https://yewyjfcrwwmftovbobwl.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlld3lqZmNyd3dtZnRvdmJvYndsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2NTk0MDYsImV4cCI6MjA5OTIzNTQwNn0.-IEcT_EfGqxjS4AAIKIbmTOonaXtF0MorQ74hEXzVrQ';
 
