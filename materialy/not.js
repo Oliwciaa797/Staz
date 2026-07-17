6c36ad7665b49911d1331bcc31a9db2b2f522bbf
Oliwciaa797 <pawlikolwika31@gamil.com>
2026-07-17 12:31:20 +0200

    fixed

--- materialy/not.js
+++ materialy/not.js
@@ -340,8 +340,6 @@ async function toggleVisibility(id, currentlyPublic){
 }
 document.addEventListener("DOMContentLoaded", () => {
 
-    loadUser();
-
     const menuBtn = document.getElementById("menuBtn");
     const sidebar = document.getElementById("sidebar");
 
