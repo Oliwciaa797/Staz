d754870da549880cb1707103016ca99d9ed552e1
Oliwciaa797 <pawlikoliwka31@gmail.com>
2026-07-26 22:59:36 +0200

    changes

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // To automatycznie serwuje WSZYSTKIE pliki z folderu (css, js, obrazki, inne strony)
-app.use(express.static(path.join(__dirname, 'strona-startowa')));
+app.use(express.static(path.join(__dirname, './strona startowa')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'strona-startowa', 'start.html'));
+  res.sendFile(path.join(__dirname, './strona startowa', 'start.html'));
 });
 
 app.listen(PORT, () => {
