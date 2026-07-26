409c55b7d331fdd8f67ea96cc86934ee8d0da8e6
Oliwciaa797 <pawlikoliwka31@gmail.com>
2026-07-26 22:40:23 +0200

    change name

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // To automatycznie serwuje WSZYSTKIE pliki z folderu (css, js, obrazki, inne strony)
-app.use(express.static(path.join(__dirname, 'strona startowa')));
+app.use(express.static(path.join(__dirname, 'strona-startowa')));
 
-// Strona startowa przy wejściu na główny adres
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'strona startowa', 'start.html'));
+  res.sendFile(path.join(__dirname, 'strona-startowa', 'start.html'));
 });
 
 app.listen(PORT, () => {
   console.log(`Serwer działa na porcie ${PORT}`);
 });
+
