f8a703ef1fc271bf2c235c3c3b04d7f37e9c108a
Oliwciaa797 <pawlikoliwka31@gmail.com>
2026-07-26 22:29:47 +0200

    ok

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// To automatycznie serwuje WSZYSTKIE pliki z folderu (css, js, obrazki, inne strony)
+app.use(express.static(path.join(__dirname, 'strona startowa')));
+
+// Strona startowa przy wejściu na główny adres
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'strona startowa', 'start.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Serwer działa na porcie ${PORT}`);
+});
