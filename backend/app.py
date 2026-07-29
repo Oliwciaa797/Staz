4486ebb1fd3c50b7d4e89de7128f98ca7faaad60
Diana <vattrysh@gmail.com>
2026-07-29 10:15:40 +0200

    proxy

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,33 @@
-import os
-from flask import Flask
-from flask_cors import CORS
+from urllib.parse import urlparse, parse_qs
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.proxies import WebshareProxyConfig
 
-from routes.youtube import youtube
-from routes.quiz import quiz
-
-app = Flask(__name__)
-
-CORS(app)
-
-app.register_blueprint(youtube)
-app.register_blueprint(quiz)
-
-if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000))
+api = YouTubeTranscriptApi(
+    proxy_config=WebshareProxyConfig(
+        proxy_username="hwolfcgd",
+        proxy_password="7ijb3ggn1t6z",
     )
+)
+
+def get_video_id(url):
+    parsed = urlparse(url)
+
+    if parsed.hostname == "youtu.be":
+        return parsed.path[1:]
+
+    if parsed.hostname in ("youtube.com", "www.youtube.com"):
+        return parse_qs(parsed.query)["v"][0]
+
+    raise ValueError("Invalid YouTube URL")
+
+
+def get_transcript(url):
+    video_id = get_video_id(url)
+
+    transcript_list = api.list(video_id)
+
+    transcript = transcript_list.find_transcript(["pl", "en"])
+
+    data = transcript.fetch()
+
+    return " ".join(item.text for item in data)
